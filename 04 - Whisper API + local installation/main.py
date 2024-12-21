5787de1bb2ef72b5a62a4ef5a36d93b75f19c78a
fakhriddin.ibragimov <ifakhriddin68@gmail.com>
2024-12-21 09:43:42 +0500

    redone

--- 04 - Whisper API + local installation/main.py
+++ 04 - Whisper API + local installation/main.py
@@ -2,7 +2,7 @@ import whisper
 import re
 
 model = whisper.load_model("base", device="cuda")
-result = model.transcribe("Adam_Lambert__Whataya_Want_from_Me.mp3")
+result = model.transcribe("ITPU_MS_Degree_Session_5_Meeting_Recording.mp3")
 transcript = '\n'.join(re.split('[?,.A-Z] ', result['text']))
 print(transcript)
 
