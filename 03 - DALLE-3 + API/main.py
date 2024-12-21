68b8b42bb8b4c0d4cff466f89c294e821bc0d3d2
foxstudent106639 <ifakhriddin68@gmail.com>
2024-12-21 10:15:28 +0500

    dalle3 api is redone

--- 03 - DALLE-3 + API/main.py
+++ 03 - DALLE-3 + API/main.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 from os import getenv
+import requests
 
 load_dotenv()
 
@@ -8,13 +9,26 @@ OPEN_AI_API_KEY = getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
-response = client.images.generate(
-    model="dall-e-3",
-    prompt="Chilling Capybara",
-    size="1024x1024",
-    quality="standard",
-    n=1,
-)
+user_prompt = input('Input : ')
 
-image_url = response.data[0].url
-print(image_url)
+styles = ['Black and white', 'Chilling', 'Realistic', 'Cartoon', 'Giantism', 'Monumental', 'Surrealistic',
+          'Cyberpunk', 'Biopunk']
+
+for style in styles:
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=user_prompt + '. In ' + style + ' style',
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+    print(user_prompt + '__in_' + style + ' style. URL: ' + image_url)
+    try:
+        img_data = requests.get(image_url).content
+        with open(user_prompt + '__in_' + style + '_style' + '.png', 'wb') as handler:
+            handler.write(img_data)
+    except Exception as err:
+        print('Could not download the image: ' + image_url)
+        print(err)
