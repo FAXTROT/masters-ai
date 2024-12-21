2b3e58a3bce0414026ff4ae2cf51467c691863e5
fakhriddin.ibragimov <ifakhriddin68@gmail.com>
2024-12-21 10:32:25 +0500

    whisper task: delimeters bug fixed

--- 04 - Whisper API + local installation/main.py
+++ 04 - Whisper API + local installation/main.py
@@ -3,7 +3,7 @@ import re
 
 model = whisper.load_model("base", device="cuda")
 result = model.transcribe("ITPU_MS_Degree_Session_5_Meeting_Recording.mp3")
-transcript = '\n'.join(re.split('[?,.A-Z] ', result['text']))
+transcript = '\n'.join(re.findall('.*?[.!?]', result['text']))
 print(transcript)
 
 with open('transcript.txt', 'w') as f:
